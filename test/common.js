e6566232b2f4653121b3a03ff9294204f559a521
Nikolay Kuchumov <kuchumovn@gmail.com>
2015-11-10 00:52:18 +0300

    test case for the small hotfix

---
+++ test/common.js
@@ -0,0 +1,17 @@
+import chai from 'chai'
+import { extend, normalize_asset_path } from './../source/common'
+
+import path from 'path'
+
+chai.should()
+
+describe('common functions', function()
+{
+	it('should normalize asset paths', function()
+	{
+		const project_folder = path.join(__dirname, 'project')
+
+		normalize_asset_path(path.join(project_folder, 'folder/file.txt'), project_folder).should.equal('./folder/file.txt')
+		normalize_asset_path(path.join(project_folder, '../another/folder/file.txt'), project_folder).should.equal(path.join(project_folder, '../another/folder/file.txt'))
+	})
+})
